8733a876478ffb57b91439b507a02d2f5a29495b
s95437@pollub.edu.pl <s95437@pollub.edu.pl>
2024-10-15 12:14:58 +0200

    Initial Commit

---
+++ IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by student on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
