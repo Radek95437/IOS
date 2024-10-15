0d0bb5f8aef49a897a73b11e0aeaa8082d86b7ca
s95437@pollub.edu.pl <s95437@pollub.edu.pl>
2024-10-15 12:59:06 +0200

    Lab1

---
+++ IOS/ColorData.swift
@@ -0,0 +1,28 @@
+//
+//  ColorData.swift
+//  IOS
+//
+//  Created by student on 15.10.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorData{
+    var colorName: String
+    var colorValue: Color
+    var count: Int
+    var id: String = UUID().uuidString
+    
+    init(colorName: String, colorValue: Color, count: Int) {
+        self.colorName = colorName
+        self.colorValue = colorValue
+        self.count = count
+        self.id = UUID().uuidString
+    }
+    
+    mutating func incrementCount(){
+        self.count += 1
+    }
+    
+}
--- IOS/ContentView.swift
+++ IOS/ContentView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var arrayOfColors : [ColorData] =
+    [ColorData.init(colorName: "red", colorValue: Color.red, count:1),
+    ColorData.init(colorName: "blue", colorValue: Color.blue, count: 2),
+    ColorData.init(colorName: "yello", colorValue: Color.yellow, count: 1),
+    ColorData.init(colorName: "green", colorValue: Color.green, count: 1),
+    ColorData.init(colorName: "black", colorValue: Color.black, count: 1),
+    ColorData.init(colorName: "white", colorValue: Color.white, count: 1),
+    ColorData.init(colorName: "gray", colorValue: Color.gray, count: 1)]
+    
+    mutating func randColor() -> Color {
+        let randomColor = Int.random(in: 0..<8)
+        arrayOfColors[randomColor].incrementCount()
+        return arrayOfColors[randomColor].colorValue
+    }
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle().fill(Color.red).opacity(0.5)
+            Circle().fill(Color.blue).opacity(0.5)
+            Circle().fill().opacity(0.5)
+            
         }
         .padding()
     }
